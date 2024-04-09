f8982bc7bc699c6cf6af58c8ed1d2498ec309e81
EmmaZhang <emmazxd@gmail.com>
2024-04-09 20:09:05 +1000

    finish cartSlice addToCart reducer

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -1,6 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+function addDecimals(value) {
+  return (Math.round(value * 100) / 100).toFixed(2);
+}
+
 //getItem() -> retrieve the value associated with the key "cart" from the local storage.
+// initialState of Order Schema, cartItems = orderItems property in Order Schema
 const initialState = localStorage.getItem("cart")
   ? JSON.part(localStorage.getItem("cart"))
   : { cartItems: [] };
@@ -12,31 +17,44 @@ const cartSlice = createSlice({
     // state-> current state, action -> includes the data to be added to the cart in its payload property
     addToCart: (state, action) => {
       // item to be added to the cart
-      const item = action.payload;
-      const existItem = state.cartItems.find((i) => i._id === item._id);
+      const itemToAdd = action.payload;
+      const existItem = state.cartItems.find((i) => i._id === itemToAdd._id);
 
-      // might need to change logic  ????????????????
+      // if item to be added is already in cart, update its detail with itemToAdd, or leave it is
       if (existItem) {
         state.cartItems = state.cartItems.map((i) =>
-          i._id === existItem._id ? item : i
+          i._id === existItem._id ? itemToAdd : i
         );
       } else {
-        state.cartItems = [...state.cartItems, item];
+        state.cartItems = [...state.cartItems, itemToAdd];
       }
 
-      //calculate item price
-      state.itemsPrice = state.cartItems.reduce(
-        (acc, item) => acc + item.price * item.qty,
-        0
+      //calculate items price
+      // state.itemsPrice -> Order: itemsPrice property
+      // item.price -> item's price and qty in orderItems property in Order schema
+      state.itemsPrice = addDecimals(
+        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
       );
 
       //calculate shipping price
-
-      //calculate tax
-
+      state.shippingPrice = addDecimals(state.itemsPrice > 200 ? 0 : 15);
+      //calculate tax (10%)
+      state.taxPrice = addDecimals(Number((state.itemsPrice * 0.1).toFixed(2)));
       //caculate total
+      state.totalPrice = (
+        Number(state.itemsPrice) +
+        Number(state.shippingPrice) +
+        Number(state.taxPrice)
+      ).toFixed(2);
+
+      //store the updated state of the cart in the browser's local storage.
+      //when user revisit the page, the state is still there
+      localStorage.setItem("cart", JSON.stringify(state));
     },
   },
 });
+
+//cartSlice.actions -> enable other component can use this function.
+export const { addToCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -8,6 +8,7 @@ const store = configureStore({
   //use a reducer provided by apiSlice and associating it with a specific slice of state identified by apiSlice.reducerPath.
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
+    //access to addToCart,
     cart: cartSliceReducer,
   },
   middleware: (getDefaultMiddleware) =>
