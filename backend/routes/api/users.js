61c28ef3e91f7f1eeed929b8d017988fe89b3207
EmmaZhang <emmazxd@gmail.com>
2024-04-10 21:01:59 +1000

    update user route

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -9,10 +9,24 @@ const ensureLoggedIn = require("../../config/ensureLoggedIn");
 // GET //api/users/check-token
 router.get("/check-token", ensureLoggedIn, usersCtrl.checkToken);
 
-// POST /api/users
-router.post("/", usersCtrl.create);
-
+// General users---------------------------
 // POST /api/users/login
 router.post("/login", usersCtrl.login);
+// POST /api/users
+router.post("/", usersCtrl.create);
+//GET /api/users/profile
+router.get("/", usersCtrl.getUserProfile);
+// PUT /api/users/profile
+router.put("/profile", usersCtrl.updateUserProfile);
+
+// Admin users---------------------------
+//GET /api/users
+router.get("/users", usersCtrl.getUsers);
+//DELETE /api/users/:id
+router.delete("/:id", usersCtrl.deleteUser);
+// GET /api/users/:id
+router.get("/:id", usersCtrl.getUserById);
+//PUT /api/users/:id
+router.put("/:id", usersCtrl.updateUser);
 
 module.exports = router;
