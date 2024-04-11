55c9c957521d474d016650937f16155e0b302d1f
EmmaZhang <emmazxd@gmail.com>
2024-04-11 23:40:48 +1000

    create generateToken function in user controller to avoid repetitive

--- backend/controllers/api/users.js
+++ backend/controllers/api/users.js
@@ -11,17 +11,7 @@ async function login(req, res) {
     const user = await User.findOne({ email });
     if (user && (await user.matchPassword(password))) {
       //generate Token
-      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-        expiresIn: "60d",
-      });
-
-      // set jwt as HTTP only cookie
-      res.cookie("jwt", token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV !== "development",
-        sameSite: "strict",
-        maxAge: 60 * 24 * 60 * 60 * 1000,
-      });
+      generateToken(res, user._id);
 
       res.json({
         _id: user._id,
@@ -50,18 +40,33 @@ function logout(req, res) {
 // POST /api/users
 async function create(req, res) {
   try {
-    //The create() function is to handle the creation of a new user.
-    const user = await User.create(req.body);
-    const token = createJWT(user);
+    const { name, email, password } = req.body;
+    const oldUser = await User.findOne({ email });
 
-    // Return user information and token in the response
-    res.status(201).json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-      token,
+    if (oldUser) {
+      res.status(400);
+      throw new Error("User already exists");
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
     });
+
+    if (user) {
+      generateToken(res, user._id);
+
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+      });
+    } else {
+      res.status(400);
+      throw new Error("Invalid user data");
+    }
   } catch (err) {
     res.status(400).json(err);
   }
@@ -70,7 +75,6 @@ async function create(req, res) {
 // getUser Profile
 // GET /api/users/profile
 async function getUserProfile(req, res) {
-  console.log("req.user", req.user);
   const user = await User.findById(req.user._id);
   if (user) {
     res.json({
@@ -91,8 +95,8 @@ async function updateUserProfile(req, res) {
   const user = await User.findById(req.user._id);
 
   if (user) {
-    user.name = req.body.name || user.name;
-    user.email = req.body.email || user.email;
+    user.name = req.body.name;
+    user.email = req.body.email;
 
     if (req.body.password) {
       user.password = req.body.password;
@@ -176,16 +180,18 @@ async function updateUser(req, res) {
 }
 
 //helper function
-function createJWT(userData) {
-  //sign (in jsonwebtoken library) -> create JWTs
-  const syncToken = jwt.sign(
-    // data payload
-    { user: userData },
-    process.env.SECRET,
-    { expiresIn: "24h" }
-  );
-  console.log("syncToken", syncToken);
-  return syncToken;
+function generateToken(res, userId) {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  // Set JWT as an HTTP-Only cookie
+  res.cookie("jwt", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV !== "development", // Use secure cookies in production
+    sameSite: "strict", // Prevent CSRF attacks
+    maxAge: 60 * 24 * 60 * 60 * 1000, // 60 days
+  });
 }
 
 function checkToken(req, res) {
