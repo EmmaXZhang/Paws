e71227f6e1d09f6ade3b75d8389bf6bc1865c773
EmmaZhang <emmazxd@gmail.com>
2024-04-14 23:30:50 +1000

    change processPayment css

--- frontend/src/pages/ProcessPaymentPage/ProcessPaymentPage.jsx
+++ frontend/src/pages/ProcessPaymentPage/ProcessPaymentPage.jsx
@@ -1,9 +1,11 @@
 import { useParams } from "react-router-dom";
-import { Row, Col, ListGroup, Image } from "react-bootstrap";
+import { Row, Col, ListGroup, Image, Card } from "react-bootstrap";
 import Loader from "../../components/Loader/Loader";
 import { useGetOrderByIdQuery } from "../../slices/ordersApiSlice";
 import paymentImg from "/images/payment.png";
 import OrderDetail from "../../components/OrderDetail/OrderDetail";
+import img from "/images/processPayment.jpeg";
+import "./ProcessPaymentPage.css";
 
 const ProcessPaymentPage = () => {
   const { id } = useParams();
@@ -13,33 +15,51 @@ const ProcessPaymentPage = () => {
   return isLoading ? (
     <Loader />
   ) : (
-    <>
+    <div>
       <Row>
         <Col md={6}>
-          <ListGroup.Item>
-            <p>
-              <strong>Order: </strong> {order._id}
-            </p>
-            <p>
-              <strong>Name: </strong> {order.user.name}
-            </p>
-            <p>
-              <strong>Email: </strong>
-              {order.user.email}
-            </p>
-            <p>
-              <strong>Address:</strong>
-              {order.shippingAddress.address}, {order.shippingAddress.city} {order.shippingAddress.postalCode},{" "}
-              {order.shippingAddress.country}
-            </p>
-          </ListGroup.Item>
-          <OrderDetail order={order} />
+          <Card style={{ border: "none", backgroundColor: "RGB(249, 243, 241)" }}>
+            <Card.Body>
+              <div className="d-flex justify-content-between">
+                <div>
+                  <Image
+                    variant="left"
+                    src={img}
+                    roundedCircle
+                    style={{ width: "140px", height: "140px", marginRight: "10px" }}
+                  />
+                </div>
+
+                <Card.Text>
+                  <ListGroup.Item style={{ backgroundColor: "RGB(249, 243, 241)" }}>
+                    <p>
+                      <strong>Order: </strong> {order._id}
+                    </p>
+                    <p>
+                      <strong>Name: </strong> {order.user.name}
+                    </p>
+                    <p>
+                      <strong>Email: </strong> {order.user.email}
+                    </p>
+                    <p>
+                      <strong>Address:</strong> {order.shippingAddress.address}, {order.shippingAddress.city}{" "}
+                      {order.shippingAddress.postalCode}, {order.shippingAddress.country}
+                    </p>
+                  </ListGroup.Item>
+                </Card.Text>
+              </div>
+            </Card.Body>
+          </Card>
+          <div className="processPayment mt-5">
+            <OrderDetail order={order} />
+          </div>
         </Col>
+
         <Col md={6}>
           <Image src={paymentImg} />
         </Col>
       </Row>
-    </>
+    </div>
   );
 };
 
