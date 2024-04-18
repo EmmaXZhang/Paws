2cc4838e5517a88852c7bca7ca4cc44ad5de6246
EmmaZhang <emmazxd@gmail.com>
2024-04-18 20:41:57 +1000

    change productPriceCard style

---
+++ frontend/src/components/ProductPriceCard/ProductPriceCard.jsx
@@ -0,0 +1,75 @@
+/* eslint-disable react/prop-types */
+import { Form, Row, Col, ListGroup, Card, Button } from "react-bootstrap";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../slices/cartSlice";
+import "./ProductPriceCard.css";
+
+const ProductPriceCard = ({ product }) => {
+  const [quantity, setQuantity] = useState(1);
+  const dispatch = useDispatch();
+
+  function addToCartHandler() {
+    // product -> data:product
+    // merge proudct and quantity into one object, which is the payload in the action
+    dispatch(addToCart({ ...product, quantity }));
+  }
+
+  function increaseQty() {
+    if (quantity < product.countInStock) {
+      setQuantity(quantity + 1);
+    }
+  }
+
+  function decreseQty() {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  }
+
+  return (
+    <Card className="productPriceCard">
+      <ListGroup variant="flush">
+        <p>PAWS</p>
+        <h3>{product.name}</h3>
+
+        <h3>${product.price}</h3>
+        <ListGroup horizontal>
+          <ListGroup.Item>{product.countInStock > 0 ? "In Stock" : "Out Of Stock"}</ListGroup.Item>
+          <ListGroup.Item>FREE 60 DAY RETURNS</ListGroup.Item>
+        </ListGroup>
+
+        {product.countInStock > 0 && (
+          <ListGroup.Item>
+            <Row>
+              <Col>
+                <div className="quantity-field">
+                  <button className="value-button decrease-button" onClick={decreseQty} title="Azalt">
+                    -
+                  </button>
+                  <div className="number">{quantity}</div>
+                  <button className="value-button increase-button" onClick={increaseQty} title="Arrtir">
+                    +
+                  </button>
+                </div>
+              </Col>
+              <Col>
+                <Button
+                  className="btn-block"
+                  type="button"
+                  variant="outline-dark"
+                  disabled={product.countInStock === 0}
+                  onClick={addToCartHandler}
+                >
+                  Add To Cart
+                </Button>
+              </Col>
+            </Row>
+          </ListGroup.Item>
+        )}
+      </ListGroup>
+    </Card>
+  );
+};
+
+export default ProductPriceCard;
--- frontend/src/pages/ProductPage/ProductPage.jsx
+++ frontend/src/pages/ProductPage/ProductPage.jsx
@@ -1,28 +1,19 @@
-import { useParams, useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import Rating from "../../components/Rating/Rating";
-import { useGetProductDetailsQuery } from "../../slices/productsApiSlice";
 import Loader from "../../components/Loader/Loader";
 import Message from "../../components/Message";
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { addToCart } from "../../slices/cartSlice";
-import { Form, Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
+import { Row, Col, Image, ListGroup } from "react-bootstrap";
+import "./ProductPage.css";
+import ProductPriceCard from "../../components/ProductPriceCard/ProductPriceCard";
+import { useGetProductDetailsQuery } from "../../slices/productsApiSlice";
+
+import { useParams } from "react-router-dom";
 
 export default function ProductPage() {
   // match route endpoint
   const { id } = useParams();
-  const [quantity, setQuantity] = useState(1);
 
-  const dispatch = useDispatch();
   const { data: product, isLoading, error } = useGetProductDetailsQuery(id);
-
-  function addToCartHandler() {
-    // product -> data:product
-    // merge proudct and quantity into one object, which is the payload in the action
-    dispatch(addToCart({ ...product, quantity }));
-    // navigate("/cart");
-  }
 
   return (
     <>
@@ -37,55 +28,12 @@ export default function ProductPage() {
       ) : (
         <>
           <Row>
-            <Col md={7}>
+            <Col md={7} lg={7} className="productImg">
               <Image src={product.image} alt={product.name} rounded fluid />
             </Col>
 
-            <Col md={5}>
-              <Card>
-                <ListGroup variant="flush">
-                  <p>PAWS</p>
-                  <h3>{product.name}</h3>
-
-                  <strong>${product.price}</strong>
-                  <ListGroup horizontal>
-                    <ListGroup.Item>{product.countInStock > 0 ? "In Stock" : "Out Of Stock"}</ListGroup.Item>
-                    <ListGroup.Item>FREE 60 DAY RETURNS</ListGroup.Item>
-                  </ListGroup>
-
-                  {product.countInStock > 0 && (
-                    <ListGroup.Item>
-                      <Row>
-                        <Col>
-                          <Form.Control
-                            as="select"
-                            value={quantity}
-                            onChange={(e) => setQuantity(Number(e.target.value))}
-                          >
-                            {/* gives array of inStock-1, starting from 0 */}
-                            {[...Array(product.countInStock).keys()].map((num) => (
-                              <option key={num + 1} value={num + 1}>
-                                {num + 1}
-                              </option>
-                            ))}
-                          </Form.Control>
-                        </Col>
-                        <Col>
-                          <Button
-                            className="btn-block"
-                            type="button"
-                            variant="outline-dark"
-                            disabled={product.countInStock === 0}
-                            onClick={addToCartHandler}
-                          >
-                            Add To Cart
-                          </Button>
-                        </Col>
-                      </Row>
-                    </ListGroup.Item>
-                  )}
-                </ListGroup>
-              </Card>
+            <Col md={5} lg={5}>
+              <ProductPriceCard product={product} />
             </Col>
           </Row>
           <Row>
