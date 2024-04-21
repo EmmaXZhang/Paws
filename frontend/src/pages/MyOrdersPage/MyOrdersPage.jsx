44a19887265be112b30c6c5aea580647ea48e061
EmmaZhang <emmazxd@gmail.com>
2024-04-21 22:33:21 +1000

    change seeding data

--- frontend/src/pages/MyOrdersPage/MyOrdersPage.jsx
+++ frontend/src/pages/MyOrdersPage/MyOrdersPage.jsx
@@ -45,60 +45,67 @@ const MyOrdersPage = () => {
       ) : (
         <>
           <Accordion defaultActiveKey="0">
-            {orders.map((order, index) => (
-              <Accordion.Item eventKey={index} key={index}>
-                <Accordion.Header className="myorders">
-                  <Row>
-                    <Col sm={6} md={6}>
-                      <div className="mb-2">Order Number</div>
-                      <div className="mb-2">Payment Status</div>
-                      <div className="mb-2">Order Status</div>
-                      <div className="mb-2">Create Date</div>
-                    </Col>
-                    <Col sm={6} md={6}>
-                      <div className="mb-2">{order._id}</div>
-                      {order.isPaid ? (
-                        <div className="paystatus mb-2">Paid</div>
-                      ) : (
-                        <div className="mb-2">
-                          <Link to={`/orders/${order._id}`}>Process to Pay</Link>
-                        </div>
-                      )}
+            {orders.length === 0 ? (
+              <h5 className="d-flex justify-content-center">No orders yet</h5>
+            ) : (
+              <>
+                {" "}
+                {orders.map((order, index) => (
+                  <Accordion.Item eventKey={index} key={index}>
+                    <Accordion.Header className="myorders">
+                      <Row>
+                        <Col sm={6} md={6}>
+                          <div className="mb-2">Order Number</div>
+                          <div className="mb-2">Payment Status</div>
+                          <div className="mb-2">Order Status</div>
+                          <div className="mb-2">Create Date</div>
+                        </Col>
+                        <Col sm={6} md={6}>
+                          <div className="mb-2">{order._id}</div>
+                          {order.isPaid ? (
+                            <div className="paystatus mb-2">Paid</div>
+                          ) : (
+                            <div className="mb-2">
+                              <Link to={`/orders/${order._id}`}>Process to Pay</Link>
+                            </div>
+                          )}
 
-                      {order.isDelivered ? (
-                        <div className="deliverstatus mb-2">Delivered</div>
-                      ) : (
-                        <div className="mb-2">Not Delivered</div>
-                      )}
+                          {order.isDelivered ? (
+                            <div className="deliverstatus mb-2">Delivered</div>
+                          ) : (
+                            <div className="mb-2">Not Delivered</div>
+                          )}
 
-                      <div className="mb-2">
-                        {new Date(order.createdAt).toLocaleDateString("en-US", {
-                          day: "numeric",
-                          month: "numeric",
-                          year: "numeric",
-                        })}
+                          <div className="mb-2">
+                            {new Date(order.createdAt).toLocaleDateString("en-US", {
+                              day: "numeric",
+                              month: "numeric",
+                              year: "numeric",
+                            })}
+                          </div>
+                        </Col>
+                      </Row>
+                    </Accordion.Header>
+                    <Accordion.Body>
+                      <OrderDetail order={order} />
+                      <div className="d-flex justify-content-between">
+                        <b></b>
+                        <Button
+                          className="mt-4"
+                          variant="outline-danger"
+                          size="sm"
+                          onClick={() => cancelOrderHandler(order._id)}
+                          disabled={order.isPaid}
+                        >
+                          Cancel Order
+                        </Button>
+                        {deletingOrder && <Loader />}
                       </div>
-                    </Col>
-                  </Row>
-                </Accordion.Header>
-                <Accordion.Body>
-                  <OrderDetail order={order} />
-                  <div className="d-flex justify-content-between">
-                    <b></b>
-                    <Button
-                      className="mt-4"
-                      variant="outline-danger"
-                      size="sm"
-                      onClick={() => cancelOrderHandler(order._id)}
-                      disabled={order.isPaid}
-                    >
-                      Cancel Order
-                    </Button>
-                    {deletingOrder && <Loader />}
-                  </div>
-                </Accordion.Body>
-              </Accordion.Item>
-            ))}
+                    </Accordion.Body>
+                  </Accordion.Item>
+                ))}
+              </>
+            )}
           </Accordion>
         </>
       )}
